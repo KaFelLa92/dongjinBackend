437201d1f9d1a794b964a595e52d58d9ca8cac2a
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-07-08 18:18:10 +0900

    백준 빡준

--- src/backJun/Main.java
+++ src/backJun/Main.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt(); // 시계 시 변수
+        int B = scan.nextInt(); // 시계 분 변수
+        int C = scan.nextInt(); // 필요한 분 변수
+
 
 
     }
