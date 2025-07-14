cdb46b2971aa1ba77ad2cdb2873401e780f4211a
KaFelLa92 <choidj10@gmail.com>
2025-07-14 20:03:08 +0900

    예제 1회 반복 완료

---
+++ src/practice/Example7/AppStart.java
@@ -0,0 +1,9 @@
+package practice.Example7;
+
+import practice.Example7.view.BoardView;
+
+public class AppStart {
+    public static void main(String[] args) {
+        BoardView.getInstance().index();
+    }
+}
