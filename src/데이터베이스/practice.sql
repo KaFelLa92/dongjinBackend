7b311a356d33e6789048c3f872e986c320b7c6ad
KaFelLa92 <choidj10@gmail.com>
2025-07-17 12:33:05 +0900

    INSERT SELECT UPDATE DELETE 연습중

--- src/데이터베이스/practice.sql
+++ src/데이터베이스/practice.sql
@@ -11,11 +11,12 @@ create table tableName(
 
 # [1] Insert
 # (1) 특정 속성에 레코드 추가
-insert into tableName(props1, props2) values(1, 2);
+insert into tableName(props1 , props2) values(1 , 2);
 # (2) 속성 아구가 딱 맞을때
-insert into tableName values(1, 2);
+insert into tableName values(1 , 2);
 # (3) 여러 레코드 추가
-insert into tableName values(1, 2) , (1, 2) , (1, 2);
+insert into tableName values (1 , 2) , (1 , 2), (1 , 2);
+insert into tableName(props1, props2) values(1 , 2) , (1 , 2) , (1 , 2);
 
 # [2] Select
 # (1) 전체 테이블 조회
@@ -34,4 +35,7 @@ update tableName set props1 = 3;
 update tableName set props2 = 3 where props1 = 3;
 
 # [4] Delete
+# (1) 전체 레코드 삭제
 delete from tableName;
+# (2) 특정 속성 조건에 따른 레코드 삭제
+delete from tableName where props2 = 3;
